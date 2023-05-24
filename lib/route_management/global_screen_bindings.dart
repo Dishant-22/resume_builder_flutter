5279b7e8dafcd157abb27d831fb948f696e0ff9e
hopetechnolab <hopetechnolab@gmail.com>
2023-05-24 21:37:02 +0530

    Add Controller to global binding

--- lib/route_management/global_screen_bindings.dart
+++ lib/route_management/global_screen_bindings.dart
@@ -1,9 +1,11 @@
 import 'package:get/get.dart';
+import 'package:resume_builder_flutter/controller/home_controller.dart';
 import 'package:resume_builder_flutter/controller/splash_controller.dart';
 
 class GlobalScreenBindings implements Bindings {
   @override
   void dependencies() {
     Get.lazyPut<SplashController>(() => SplashController());
+    Get.lazyPut<HomeController>(() => HomeController());
   }
 }
